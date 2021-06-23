0f717f7d74ceb20e9dfff44f21387cc157f3d5f7
Arvin Alvea <arvinalvea@gmail.com>
2021-06-23 13:29:16 -0400

    Added phrases in Game.js

--- js/Game.js
+++ js/Game.js
@@ -5,7 +5,13 @@
 class Game {
     constructor() {
         this.missed = 0;
-        this.phrases = [];
+        this.phrases = [
+            new Phrase("May the force be with you"),
+            new Phrase("Show me the money"),
+            new Phrase("My precious"),
+            new Phrase("Nobody puts Baby in a corner"),
+            new Phrase("Carpe Diem"),
+        ];
         this.activePhrase = null;
     }
 }
